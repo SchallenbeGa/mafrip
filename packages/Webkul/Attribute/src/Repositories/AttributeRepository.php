7dcc4ecc4e7d6212b982730ec160160ac2c182a7
Gabriel <gabriel.schallenberger@outlook.com>
2024-01-01 18:02:21 +0100

    trz

--- packages/Webkul/Attribute/src/Repositories/AttributeRepository.php
+++ packages/Webkul/Attribute/src/Repositories/AttributeRepository.php
@@ -136,7 +136,7 @@ class AttributeRepository extends Repository
      */
     public function getFilterableAttributes()
     {
-        return $this->model->with(['options', 'options.translations'])->where('is_filterable', 1)->get();
+        return $this->model->get();
     }
 
     /**
