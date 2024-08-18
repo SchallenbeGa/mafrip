be98ae71041764ec295904e12e228b53cb1a0e51
Gabriel <gabriel.schallenberger@outlook.com>
2024-08-18 21:58:33 +0200

    m

--- packages/Webkul/Shop/src/Resources/views/search/index.blade.php
+++ packages/Webkul/Shop/src/Resources/views/search/index.blade.php
@@ -137,6 +137,7 @@
 
                         <!-- Load More Button -->
                         <button
+                         style="margin-top: 1rem;margin-bottom: 4rem;"
                             class="secondary-button mx-auto mt-[60px] block w-max rounded-2xl px-11 py-3 text-center text-base max-md:rounded-lg max-md:text-sm max-sm:mt-7 max-sm:px-7 max-sm:py-2"
                             @click="loadMoreProducts"
                             v-if="links.next"
